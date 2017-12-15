3147bf96f461c04f46d7f08540f4d7b6f8fad167
root <root@localhost.localdomain>
2017-12-14 20:03:29 -0500

    day4 part 2 added

--- day4.pl
+++ day4.pl
@@ -1,5 +1,22 @@
 use strict;
 use warnings;
+use Word::Anagram;
+use Data::Dumper;
+
+sub checkAnagram{
+	my @list = @_;
+	my $obj = Word::Anagram->new;
+	
+	for my $i (@list){
+		my $anag = $obj->find_word_in($i, \@list);
+		if (@$anag > 1){
+			print "I should be breaking here.\n";
+			return 0;
+
+		}
+	}
+	return 1;
+}
 
 sub checkValid {
         my @list = @_;
@@ -15,10 +32,15 @@ sub checkValid {
 my $file = 'day4.txt';
 open (my $fh, '<:encoding(UTF-8)', $file) or die "Unable to open $file\n";
 my $check_sum = 0;
+my $valid_checks = 0;
 
 while (my $row = <$fh>) {
         chomp $row;
 	my @row_split = split / /, $row;
 	$check_sum += checkValid(@row_split);
+	if (checkAnagram(@row_split) == 1){
+		$valid_checks += 1;
+	}
 } 
-print "Valid Passwords: $check_sum";
+print "Valid Passwords: $check_sum\n";
+print "Valid Checks: $valid_checks";
